f24d3c24b9f70bcbf37fb252a3fe9508fa7d80e2
SolarDaz3 <jeremygalan@hotmail.com>
2025-09-20 13:32:44 -0400

    dark theme toggle

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-// 
+let isModalOpen = false;
+let contrastToggle = false;
 
-// 
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if (contrastToggle){
+        document.body.classList += " dark-theme"
+}
+else{
+    document.body.classList.remove("dark-theme")
+}
+}
 
   function contact (event){
     event.preventDefault();
@@ -25,10 +34,10 @@
 
     })
 }
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen ){
-        isModalOpen = false;
+        
         return document.body.classList.remove("modal--open");
     }
     isModalOpen = !isModalOpen;
